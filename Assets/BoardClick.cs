3dba8b2882481123075f7fa65970c91729b13e4f
dds4120 <dds4120@naver.com>
2016-06-27 17:35:02 +0900

    board click, dungeon select Event

---
+++ Assets/BoardClick.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BoardClick : MonoBehaviour {
+
+
+	public Image boardQuest;//board PopUpImage
+	public UserInterfaceManager ui;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		boardQuest = transform.Find ("BoardCanvas").Find ("BoardClickEvent").GetComponent<Image>();
+		ui = GameObject.FindWithTag ("MainUI").GetComponent<UserInterfaceManager> ();
+		ui.ControlEnterDungeon (false);
+		ControlBoardImage (false);
+	}
+
+	public void ControlBoardImage(bool state)
+	{
+		boardQuest.enabled = state;
+	}
+
+	void OnMouseEnter()
+	{
+		ControlBoardImage (true);	
+
+	}
+	void OnMouseExit()
+	{
+		ControlBoardImage (false);
+
+	}
+	void OnMouseDown()
+	{
+		if (!ui.OnEnterDungeon)
+		{
+			ControlBoardImage (false);
+			ui.ControlEnterDungeon (true);
+			Debug.Log ("DungeonIn");
+			
+		} 
+
+	}
+
+
+}
---
+++ Assets/BoardScript/UserInterfaceManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInterfaceManager : MonoBehaviour
+{
+	//child UI
+//	public GameObject inventory;
+//	public GameObject skillUI;
+//	public GameObject status;
+	public GameObject enterDungeon;
+
+	//initialize this script
+	void Start( )
+	{
+//		inventory = GameObject.Find( "Inventory" );
+//		skillUI = GameObject.Find( "SkillUI" );
+//		status = GameObject.Find( "Status" );
+		enterDungeon = GameObject.Find( "EnterDungeon" );
+		Debug.Log (enterDungeon);
+	}
+	public bool OnEnterDungeon
+	{
+		get 
+		{
+			return enterDungeon.activeInHierarchy;
+		}
+	}
+
+	// Update is called once per frame
+//	void Update( )
+//	{
+//		Debug.Log( inventory );
+//	}
+//
+//	// control UI 
+//	//inventory
+//	public void ControlInventory(bool state)
+//	{
+//		inventory.SetActive( state );
+//	}
+//	//skill ui
+//	public void ControlSkillUI(bool state)
+//	{
+//		skillUI.SetActive( state );
+//	}
+//	//status ui
+//	public void ControlStatus(bool state)
+//	{
+//		status.SetActive( state );
+//	}
+//	// enter dungeon
+	public void ControlEnterDungeon(bool state)
+	{
+		enterDungeon.SetActive( state );
+	}
+}
